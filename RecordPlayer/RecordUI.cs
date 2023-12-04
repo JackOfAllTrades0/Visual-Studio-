9986499ea6e130236b9f8404861be8ba4ccbb5ae
Jack Johnson <Jackaj2007@outlook.com>
2023-12-04 18:36:54 +0000

    Almost there, just need to work on reading the file and the UI

--- RecordPlayer/RecordUI.cs
+++ RecordPlayer/RecordUI.cs
@@ -9,10 +9,26 @@ namespace RecordPlayer
 {
     class RecordUI
     {
+        static string UserInteract()
+        {
+            string choice = "";
+            string[] options = { "Cr","Lg","Sv","Ld"};
+            do
+            {
+                Console.Write(@"Cr - Create new entry in the record
+Lg - Log the contents of the record to the console (print it)
+Sv - Save the Contents of the record under a name to a file
+Ld - Load the data of a Record from the file");
+                choice = Console.ReadLine();
+
+            } while (!options.Any(choice.Equals));
+            return choice;
+
+        }
         static void Main(string[] args)
         {
             StudentRecords RecordIt = new StudentRecords();
-            RecordIt.CreateRecord();
+            
 
             Console.ReadKey();
         }
--- RecordPlayer/StudentRecords.cs
+++ RecordPlayer/StudentRecords.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,19 @@ namespace RecordPlayer
 {
     class StudentRecords
     {
+        public static string ClassName;
+        public StreamWriter sw = new StreamWriter("Class_Records.txt",append:true);
+        public StreamReader sr = new StreamReader("Class_Records.txt");
         public record Person(string ID, string FirstName, string Surname, string TutorGroup, string Score); // 'Blue prints' for a record containing fields relative to a student
         public static Person[] StuData = new Person[]
         {
 
         };
+
+        public StudentRecords(string nameOfClass ) 
+        {
+            ClassName = nameOfClass;
+        }
 
         public void CreateRecord()
         {
@@ -21,12 +30,45 @@ namespace RecordPlayer
             StuData = StuData.Append(new Person(fields[0], fields[1], fields[2], fields[3], fields[4])).ToArray();
         }
 
-        public void LogRecords(string ClassName)
+        public void LogRecords()
         {
             Console.WriteLine($"{ClassName}:");
             foreach (var student in StuData)
             {
                 Console.WriteLine(student);
+            }
+        }
+
+        public void SaveCurrentRecord()
+        {
+            using (sw)
+            {
+                sw.WriteLine("\n" + ClassName+":");
+                foreach (var student in StuData)
+                {
+                    sw.WriteLine("-" + student);
+                }
+                sw.WriteLine("End of Class Record");
+            }
+        }
+
+        public void LoadRecord()
+        {
+            List<string> FileContents = new List<string> { };
+            try
+            { 
+                using (sr)
+                {
+                    while (sr.Peek() > -1)
+                    {
+                        FileContents.Append(sr.ReadLine());
+                    }
+
+                }
+            }
+            catch(Exception e)
+            {
+                
             }
         }
 
